738fa7acc8901aa631390a2a16296a155ef8d338
aidenseibel <aiden.seibel@gmail.com>
2023-10-21 14:40:46 -0500

    Initial Commit

---
+++ hacktx-2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hacktx-2023
+//
+//  Created by Aiden Seibel on 10/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hacktx-2023UITests/hacktx_2023UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  hacktx_2023UITestsLaunchTests.swift
+//  hacktx-2023UITests
+//
+//  Created by Aiden Seibel on 10/21/23.
+//
+
+import XCTest
+
+final class hacktx_2023UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
